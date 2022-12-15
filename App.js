f7b72f3b907a60fe744c4f28fe3484ef65fee2d9
JoaoVDS21 <joao.programmerS2@gmail.com>
2022-12-15 00:11:23 -0300

    Prototipação tela preload e bottom navigator

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+
+import MainStack from './src/stacks/MainStack'
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
   );
 }
 
